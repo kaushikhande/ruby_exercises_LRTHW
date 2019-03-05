cfe7cdcd95e26339e74fdd7242ec339af5baf6be
kaushikhande <kaushikhande302@gmail.com>
2019-03-05 15:28:16 +0530

    added-function

--- ruby_2.rb
+++ ruby_2.rb
@@ -23,3 +23,27 @@ print_a_line(1, current_file)
 print_a_line(2, current_file)
 
 print_a_line(3, current_file)
+
+puts poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+def secret_formula(started)
+  jelly_beans = started * 2
+  jars = jelly_beans * 1000 * 6344
+  crates = jars * 100 * 400
+  return jelly_beans, jars, crates
+end
+
+beans, jars, crates = secret_formula(200)
+
+puts beans
+
+puts jars
+
+puts crates
